72bb2b2335a85da09e475824b0852a939ac0074d
V <vendicated@riseup.net>
2023-05-13 02:45:28 +0200

    Update description

--- src/plugins/BetterRoleDot/meta.js
+++ src/plugins/BetterRoleDot/meta.js
@@ -2,6 +2,6 @@
  * @name BetterRoleDot
  * @author Vendicated
  * @authorId 343383572805058560
- * @description Allows you to use role dots while still having coloured role names
+ * @description Allows you to use role dots while still having coloured role names! Also makes the role dot copy the role colour on click
  * @version 1.0.0
  */
