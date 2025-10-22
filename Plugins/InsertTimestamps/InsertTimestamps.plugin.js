6469f880ccbe074a2e9d13d8d7e8045c8ec27291
Huderon <huderon.mcs@gmail.com>
2025-10-23 08:11:22 +1100

    InsertTimestamps: fix version

--- Plugins/InsertTimestamps/InsertTimestamps.plugin.js
+++ Plugins/InsertTimestamps/InsertTimestamps.plugin.js
@@ -3,7 +3,7 @@
  * @author Vendicated
  * @authorId 343383572805058560
  * @description Allows you to insert timestamp markdown with a convenient chat bar button
- * @version 1.0.13
+ * @version 1.0.12
  */
 
 "use strict";
